5ee82baffb1f4515a69728437c55fce1a645d6fd
Kj-kja <89133220+Kj-kja@users.noreply.github.com>
2024-06-11 21:37:26 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-# config.py
+import os
+
 class Config:
-    DEBUG = True  # Set to False in production
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgre:Kriti8532@localhost/blogging_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Kriti8532@localhost/blogging_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
