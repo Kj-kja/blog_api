cd95cefa09141637ea0f0ebd4fbe1c1a544ed52a
Kj-kja <89133220+Kj-kja@users.noreply.github.com>
2024-06-11 21:38:07 +0530

    models.py

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
-# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self):
-        return f'<BlogPost {self.title}>'
+    author = db.relationship('User', backref=db.backref('posts', lazy=True))
