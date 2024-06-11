05e7d3a3d3943cd4781e63edbf8e571160f7fc13
Kj-kja <89133220+Kj-kja@users.noreply.github.com>
2024-06-11 21:36:44 +0530

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-# app.py
-# app.py
 from flask import Flask
-from config import Config  # Import Config from config.py
-from models import db  # Import db from models.py
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from auth import auth_bp
 from routes import blog_posts_bp
+from models import db
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    
-    db.init_app(app)
-    app.register_blueprint(blog_posts_bp)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    return app
+db.init_app(app)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(blog_posts_bp)
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5001)
